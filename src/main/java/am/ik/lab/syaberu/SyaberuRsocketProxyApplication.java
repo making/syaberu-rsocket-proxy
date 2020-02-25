4cf1a6bd5c9be13c0ea44438c956bcdc3fbae8c3
Toshiaki Maki <tmaki@pivotal.io>
2020-02-26 02:52:12 +0900

    initial commit

---
+++ src/main/java/am/ik/lab/syaberu/SyaberuRsocketProxyApplication.java
@@ -0,0 +1,13 @@
+package am.ik.lab.syaberu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyaberuRsocketProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyaberuRsocketProxyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/am/ik/lab/syaberu/SyaberuRsocketProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package am.ik.lab.syaberu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyaberuRsocketProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
